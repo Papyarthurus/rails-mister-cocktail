c9602a530ad361771b7fae45ff4974d738d235e9
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-05-18 12:19:27 +0200

    add links and validations between models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktail
-  belongs_to :ingredient
+  belongs_to :cocktail, dependent: :destroy
+  belongs_to :ingredient, dependent: :destroy
+
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
 end
